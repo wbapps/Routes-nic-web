b1b0270701acaafea6e97a0887e486c7ce2110ae
Ernesto Ruiz <ernesto_ruiz89@hotmail.com>
2012-10-08 00:25:34 -0600

    static data

--- app/admin.py
+++ app/admin.py
@@ -2,21 +2,27 @@
 from django.contrib import admin
 from app.models import Ruta, Punto, Trayectoria, Recorrido
 
+class RutaInline(admin.TabularInline):
+	model = Trayectoria
+	extra = 1
+
 class RutaAdmin(admin.ModelAdmin):
 	ordering =  ['id']
+	inlines = [RutaInline]
 	
 class PuntoAdmin(admin.ModelAdmin):
-	list_display	= ('punto','lat','lon')
-	search_fields	= ('punto',)
+	list_display	= ['punto','lat','lon']
+	search_fields	= ['punto',]
 	ordering =  ['punto']
 
 class TrayectoriaAdmin(admin.ModelAdmin):
-	list_display	= ('ruta_id','punto_id','orden')
-	search_fields	= ('ruta_id__ruta','punto_id__punto')
-	list_filter		= ('ruta_id',)
+	list_display	= ['ruta_id','punto_id','orden']
+	search_fields	= ['ruta_id__ruta','punto_id__punto']
+	list_filter		= ['ruta_id',]
 	
 class RecorridoAdmin(admin.ModelAdmin):
-	list_display	= ('ruta','lat','lon','orden')	
+	list_display	= ['ruta','lat','lon','orden']	
+	
 
 admin.site.register(Ruta, RutaAdmin)
 admin.site.register(Punto, PuntoAdmin)

5df3f1c4f3d47eab2b158dbe7e0ea27ef0a355fd
Ernesto Ruiz <ernesto_ruiz89@hotmail.com>
2012-10-19 00:21:56 -0600

    migrations

--- settings.py
+++ settings.py
@@ -130,6 +130,10 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.contrib.messages.context_processors.messages"
 
     )
+	
+FIXTURE_DIRS = (
+   os.path.join(SITE_ROOT,'app/migrations/'),
+)
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error.

4ee8a27313961afb06e2e028b37a4c8309339022
Ernesto Ruiz <ernesto_ruiz89@hotmail.com>
2012-11-11 18:06:38 -0600

    pissed off with Heroku

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls.defaults import patterns, url, include
+from django.conf.urls.defaults import patterns, include, url
+from django.contrib import admin
+from dajaxice.core import dajaxice_autodiscover, dajaxice_config
+admin.autodiscover()
+dajaxice_autodiscover()
 
-urlpatterns = patterns('app.views',
-	url(r'^$','rutas_view', name='rutas_view'),
-	url(r'^puntos/$','puntos_view', name='puntos_view'),
+urlpatterns = patterns('app.views',	
+    url(r'^admin/', include(admin.site.urls)), 
+    url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
+    url(r'^$','rutas_view', name='rutas_view'),
+    url(r'^puntos/$','puntos_view', name='puntos_view'),
     url(r'^buscar/$','buscar_view', name='buscar_view'),
 )

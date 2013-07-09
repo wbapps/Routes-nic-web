b5bbbe7698afbaf6abdb28ee10170ccacc7c5523
Ernesto Ruiz <ernesto_ruiz89@hotmail.com>
2013-07-08 20:34:44 -0600

    Delete example code

--- app.js
+++ app.js
@@ -22,40 +22,6 @@ app.configure (function (){
 
 app.get("/", controllers.pages.home);
 
-app.get("/hi", function (req, res){
-   var message = [
-        "<h1>Hello, Express!</h1>",
-        "<p>Welcome to Building web apps in node.js</p>",
-        "<p>you'll love Express because it's</p>",
-        "<ul><li>fast</li>",
-        "<li>fun</li>",
-        "<li>flexible</li></ul>"].join("\n");
-    res.send(message);
-});
-
-/*
-app.get("/users/:userId", function (req, res){
-    res.send("<h1> Hello, User #" + req.params.userId + "!");
-})
-*/
-app.post("/users", function (req, res){
-    res.send("Creating a new user with the name " + req.body.username + ".");
-});
-
-app.get(/\/users\/(\d*)\/?(edit)?/, function (req, res){
-  // /users/10
-  // /users/10/
-  // /users/10/edit
-  var message = "user #" + req.params[0] + "'s profile";
-
-  if(req.params[1] === 'edit'){
-    message = "Editing " + message;
-  } else {
-    message = "Viewing " + message;
-  }
-  res.send(message);
-});
-
 http.createServer(app).listen(app.get('port'), function (){
   console.log("Express server listening on port " + app.get('port'));
 });
